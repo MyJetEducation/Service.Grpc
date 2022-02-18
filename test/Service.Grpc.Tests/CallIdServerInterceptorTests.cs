64b93342572d8647018da13f81b904739608432b
dmitriy.b <dmitriy.b@smplt.net>
2022-02-18 15:33:58 +0500

    FIN360PMVP-157: Tests added

---
+++ test/Service.Grpc.Tests/CallIdServerInterceptorTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace Service.Grpc.Tests
+{
+	public class CallIdServerInterceptorTests
+	{
+		private readonly Guid _callId = new("b90aa6df-98d7-4ab9-bc45-0001e3c1af88");
+
+		private Mock<IGrpcResponseCache> _grpcResponseCache;
+		private CallIdServerInterceptor _sut;
+
+		[SetUp]
+		public void Setup()
+		{
+			_grpcResponseCache = new Mock<IGrpcResponseCache>();
+			_sut = new CallIdServerInterceptor(new Mock<ILogger>().Object, _grpcResponseCache.Object);
+		}
+
+		[Test]
+		public void UnaryServerHandler_return_default_response_if_no_call_id_found_in_headers()
+		{
+			ServerCallContext context = GetTestContext();
+			var request = new TestRequestDto();
+
+			Task<TestRequestDto> response = _sut.UnaryServerHandler(request, context, (_, _) => Task.FromResult(request));
+
+			_grpcResponseCache.Verify(cache => cache.Get<TestRequestDto>(It.IsAny<Guid>()), Times.Never);
+			_grpcResponseCache.Verify(cache => cache.Set(It.IsAny<Guid>(), It.IsAny<TestRequestDto>()), Times.Never);
+
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.Result, request);
+		}
+
+		[Test]
+		public void UnaryServerHandler_return_default_response_if_call_id_found_and_cache_it()
+		{
+			ServerCallContext context = GetTestContext(new Metadata { { CallIdServerInterceptor.CallIdKey, _callId.ToString() } });
+			var request = new TestRequestDto();
+
+			_grpcResponseCache
+				.Setup(cache => cache.Get<TestRequestDto>(_callId))
+				.Returns((TestRequestDto)null);
+
+			Task<TestRequestDto> response = _sut.UnaryServerHandler(request, context, (_, _) => Task.FromResult(request));
+
+			_grpcResponseCache.Verify(cache => cache.Get<TestRequestDto>(_callId), Times.Once);
+
+			TestRequestDto responseResult = response.Result;
+			_grpcResponseCache.Verify(cache => cache.Set(_callId, responseResult), Times.Once);
+
+			Assert.IsNotNull(response);
+			Assert.AreEqual(responseResult, request);
+		}
+
+		[Test]
+		public void UnaryServerHandler_return_cached_response_if_call_id_found_and_cache_contains_response()
+		{
+			ServerCallContext context = GetTestContext(new Metadata { { CallIdServerInterceptor.CallIdKey, _callId.ToString() } });
+
+			var cachedDto = new TestRequestDto();
+			_grpcResponseCache
+				.Setup(cache => cache.Get<TestRequestDto>(_callId))
+				.Returns(cachedDto);
+
+			Task<TestRequestDto> response = _sut.UnaryServerHandler(new TestRequestDto(), context, (_, _) => Task.FromResult(new TestRequestDto()));
+
+			TestRequestDto responseResult = response.Result;
+			_grpcResponseCache.Verify(cache => cache.Set(It.IsAny<Guid>(), It.IsAny<TestRequestDto>()), Times.Never);
+
+			Assert.IsNotNull(response);
+			Assert.AreEqual(responseResult, cachedDto);
+		}
+
+		private static ServerCallContext GetTestContext(Metadata requestHeaders = null) => TestServerCallContext.Create(
+			"method", "localhost", DateTime.Now.AddMinutes(30), requestHeaders ?? new Metadata()
+			, CancellationToken.None, "localhost", null, null, _ => Task.CompletedTask
+			, () => new WriteOptions(), _ => { });
+
+		private class TestRequestDto
+		{
+		}
+	}
+}
---
+++ test/Service.Grpc.Tests/GrpcResponseCacheTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Service.Core.Client.Services;
+
+namespace Service.Grpc.Tests
+{
+	public class GrpcResponseCacheTests
+	{
+		private GrpcResponseCache _sut;
+		private Mock<ISystemClock> _systemClock;
+
+		[SetUp]
+		public void Setup()
+		{
+			_systemClock = new Mock<ISystemClock>();
+			_sut = new GrpcResponseCache(_systemClock.Object, new Mock<ILogger<GrpcResponseCache>>().Object, 200, 20);
+		}
+
+		[Test]
+		public void Get_return_null_if_nothing_cached_by_call_id()
+		{
+			Guid callId = new("ad312633-cf90-4b06-b2f2-416f431034b0");
+
+			var result = _sut.Get<TestDto>(callId);
+
+			Assert.IsNull(result);
+		}
+
+		[Test]
+		public void Get_return_dto_if_is_cached_by_call_id()
+		{
+			Guid callId = new("69835d8a-bf6f-4b3a-8d77-93bd34634cd8");
+
+			var testDto = new TestDto();
+
+			_sut.Set(callId, testDto);
+
+			var result = _sut.Get<TestDto>(callId);
+
+			Assert.AreSame(result, testDto);
+		}
+
+		[Test]
+		public void Get_return_null_after_clear_cache()
+		{
+			Guid callId = new("26e8a313-0707-4051-8784-4054fa0e12e9");
+
+			var testDto = new TestDto();
+
+			_systemClock
+				.Setup(clock => clock.Now)
+				.Returns(() => DateTime.UtcNow);
+
+			_sut.Set(callId, testDto);
+
+			Thread.Sleep(1000);
+
+			var result = _sut.Get<TestDto>(callId);
+
+			Assert.IsNull(result);
+		}
+
+		[Test]
+		public void Test_parallel()
+		{
+			Parallel.For(1, 30000, (_, _) =>
+			{
+				var callId = Guid.NewGuid();
+
+				var testDto = new TestDto();
+
+				_sut.Set(callId, testDto);
+
+				var result = _sut.Get<TestDto>(callId);
+
+				Assert.AreSame(result, testDto);
+			});
+		}
+
+
+		private class TestDto
+		{
+		}
+	}
+}
